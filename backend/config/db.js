89ecfca9e6d836fb67c18cb3ee584a8444b721ba
angelina <masteniangelina17@gmail.com>
2025-11-10 04:59:31 -0800

    last changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,8 +8,6 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {});
         console.log("MongoDB connected");
-
-        // *** 2. Вызываем функцию создания администратора ***
         await createDefaultAdmin();
         
     } catch (err) {
